11669d3e2412b5b5c311dca051fc74b2efd9cfc4
Vadim Kislov <kislov.v@gmail.com>
2022-08-23 18:29:09 +0300

    Initial Commit

---
+++ ContactListUI/ContactListUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListUIApp.swift
+//  ContactListUI
+//
+//  Created by Kislov Vadim on 23.08.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactListUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
